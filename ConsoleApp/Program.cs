cc4125a433ffee3f7657681238faf14def8e2336
cristian grimaldi <grimaldi.cristian@gmail.com>
2019-10-03 19:43:45 -0300

    promedio del contenido de un array

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,7 +6,17 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            
+            int[] array = { 18, 4, 9, 3, 2 };
+            int cuenta = 0;
+            int promedio = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                cuenta += array[i];
+            }
+            promedio = cuenta / array.Length;
+
+            Console.WriteLine(promedio);
         }
     }
 }
